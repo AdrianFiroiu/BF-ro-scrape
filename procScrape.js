ac421346ff68f2de2eb26b24866f240bfdd4b1f1
Mihai Firoiu <mihai-adrian.firoiu@harting.com>
2019-12-13 16:42:54 +0200

    Remove commented tests

--- procScrape.js
+++ procScrape.js
@@ -2,36 +2,20 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 // PCGarage
-// request('https://www.pcgarage.ro/procesoare/amd/filtre/general-serie-ryzen-5-3rd-gen/', (error, response, html) => {
-//     if (!error && response.statusCode == 200) {
-//         const $ = cheerio.load(html);
-//
-//         console.log('-------PCGarage - AMD Ryzen 5 3rd Gen-------');
-//         $('.product-box-container').each((i, el) => {
-//             const prodTitle = $(el).find('.pb-name').text();
-//             const prodPrice = $(el).find('.pb-price .price').text();
-//
-//             console.log(prodTitle, prodPrice);
-//         });
-//         console.log('--------------------------------------------');
-//     }
-// });
+request('https://www.pcgarage.ro/procesoare/amd/filtre/general-serie-ryzen-5-3rd-gen/', (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
 
-//eMAG
-// request('https://www.emag.ro/procesoare/brand/amd/filter/familie-procesor-f2666,amd-ryzen-5-v24753/vendor/emag/c?ref=lst_leftbar_6427_1', (error, response, html) => {
-//    if (!error && response.statusCode == 200) {
-//        const $ = cheerio.load(html);
-//
-//        console.log('-------eMAG - AMD Ryzen 5 3rd Gen-------');
-//        $('.card-item').each((i, el) => {
-//            const prodTitle = $(el).find('.product-title-zone a').text().replace(/\s\s+/g, '').replace('Procesor', '');
-//            const prodPrice = $(el).find('.product-new-price').text();
-//
-//            console.log(prodTitle, '-', prodPrice);
-//        });
-//        console.log('----------------------------------------');
-//    }
-// });
+        console.log('-------PCGarage - AMD Ryzen 5 3rd Gen-------');
+        $('.product-box-container').each((i, el) => {
+            const prodTitle = $(el).find('.pb-name').text();
+            const prodPrice = $(el).find('.pb-price .price').text();
+
+            console.log(prodTitle, prodPrice);
+        });
+        console.log('--------------------------------------------');
+    }
+});
 
 //CEL
 request('https://www.cel.ro/procesoare/amd/serie-i953/ryzen-5/5a-1', (error, response, html) => {
@@ -50,19 +34,3 @@ request('https://www.cel.ro/procesoare/amd/serie-i953/ryzen-5/5a-1', (error, res
         console.log('Error: ', error);
     }
 });
-
-// var url = 'https://www.cel.ro';
-// request.get({
-//     url: url,
-//     json: true,
-//     headers: {'User-Agent': 'request'}
-// }, (err, res, data) => {
-//     if (err) {
-//         console.log('Error:', err);
-//     } else if (res.statusCode !== 200) {
-//         console.log('Status:', res.statusCode);
-//     } else {
-//         // data is already parsed as JSON:
-//         console.log(data.html_url);
-//     }
-// });
